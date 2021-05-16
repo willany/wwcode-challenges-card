5863568966870254d2cfd59109f9321ba42ae116
Willany Silva <willany_lp@hotmail.com>
2021-05-16 12:09:10 -0300

    Change image name

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   ImageContainer,
 } from "./styles";
 
-import profilePicture from "./assets/willany.jpg";
+import profileImage from "./assets/willany.jpg";
 import linkedinIcon from "./assets/icons/linkedin.svg";
 import githubIcon from "./assets/icons/github.svg";
 import instagramIcon from "./assets/icons/instagram.svg";
@@ -23,7 +23,7 @@ function App() {
         <Card>
           <ImageContainer>
             <Image
-              src={profilePicture}
+              src={profileImage}
               alt="Mulher com um computador no colo"
             />
           </ImageContainer>
